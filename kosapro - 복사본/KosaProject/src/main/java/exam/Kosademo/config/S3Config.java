c389d8225ee285e4bdc4b9c9c939cc17abea2a10
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-08-02 01:37:13 +0900

    Add files via upload

---
+++ kosapro - 복사본/KosaProject/src/main/java/exam/Kosademo/config/S3Config.java
@@ -0,0 +1,23 @@
+package exam.Kosademo.config;
+
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+}
